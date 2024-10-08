22f9f83791f4773e59c7f053074941c791a9404b
EDOARDO ESPOSITO s329079 <s329079@studenti.polito.it>
2024-10-08 14:54:12 +0200

    adding router

--- client/my-app/src/index.jsx
+++ client/my-app/src/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+
+const router = createBrowserRouter([{
+  path:'/*',
+  element:<App/>
+}])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
