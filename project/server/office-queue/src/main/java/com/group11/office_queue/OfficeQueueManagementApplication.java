9efe79a02eecf7e9ba83f043c7604fff36ab114a
Bilol <53166104+Ibrokhimzodab@users.noreply.github.com>
2024-10-11 00:58:23 +0200

    Init java project

---
+++ project/server/office-queue/src/main/java/com/group11/office_queue/OfficeQueueManagementApplication.java
@@ -0,0 +1,13 @@
+package com.group11.office_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeQueueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeQueueManagementApplication.class, args);
+	}
+
+}
