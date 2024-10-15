7dd06078a27b2d0a837588cffb475917a0d3e4f9
EDOARDO ESPOSITO s329079 <s329079@studenti.polito.it>
2024-10-15 12:05:40 +0200

    add dynamic stuff

--- project/client/src/components/GetTicketFormComponent.jsx
+++ project/client/src/components/GetTicketFormComponent.jsx
@@ -1,28 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './GetTicketFormCSS.css';
 
+const INITIAL_SERVICE = [
+  1,3,4,5
+];
+
 export function GetTicketForm(props) {
     const navigate = useNavigate();
-    const [service, setService] = useState("Service 1");
-
+    const [service, setService] = useState();
+    const [serviceList, setServiceList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    let List = [];
     const handleTicketSelection = () => {
       navigate(`/ticket/${service}`);
 
     };
+    /*
+    useEffect(()=>{
+      loadService().then((my_service)=>{
+          setServiceList(my_service);
+          setLoading(false);
+      })
+      
+  }, [])
+
+  */
+
+  
+    if(loading===true){
+      console.log(INITIAL_SERVICE)
+    INITIAL_SERVICE.forEach((s)=>List.push(s))
+      setServiceList(List);
+      setLoading(false);
+    }
+      
+
   
     return (
       <div className="home-page">
         <Container className="text-center">
           <h1 className="my-5">Select Service</h1>
-          <Form.Select aria-label="services-list" onChange={ev => setService(ev.target.value)} >
-            {/*<option></option>*/}
-            <option value="Service 1">Service 1</option>
-            <option value="Service 2">Service 2</option>
-            <option value="Service 3">Service 3</option>
-            <option value="Service 4">Service 4</option>
+          {!loading && <Form.Select aria-label="services-list" onChange={ev => setService(ev.target.value)} >
+            { serviceList.map((s) => <option value={s}>{s}</option>)}
           </Form.Select>
+          }
           <Button variant="primary" size="lg" className="mx-3" onClick={handleTicketSelection}>
             Get Ticket
           </Button>
--- project/client/src/components/QueueList.jsx
+++ project/client/src/components/QueueList.jsx
@@ -38,3 +38,6 @@ export function QueueList(props) {
     </Container>
     );
   }
+
+
+  
