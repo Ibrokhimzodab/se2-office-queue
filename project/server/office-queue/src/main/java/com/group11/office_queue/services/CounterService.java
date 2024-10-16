86f310126904171861b043c7ea390bc8273e77b8
Bilol <53166104+Ibrokhimzodab@users.noreply.github.com>
2024-10-16 17:58:12 +0200

    Add: added get all queue

--- project/server/office-queue/src/main/java/com/group11/office_queue/services/CounterService.java
+++ project/server/office-queue/src/main/java/com/group11/office_queue/services/CounterService.java
@@ -23,10 +23,4 @@ public class CounterService {
         // temporary return
         return new NextCustomerDTO("FOO-1");
     }
-
-    public ServiceDTO callCustomer(String waitListCode){
-
-        //temporary return
-        return new ServiceDTO(3, "name", 10);
-    }
 }
