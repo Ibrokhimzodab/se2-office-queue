bf357cdb87b304f9c87e61f6b9300c3add59f245
Bilol <53166104+Ibrokhimzodab@users.noreply.github.com>
2024-10-16 13:03:16 +0200

    Add: added cors config

---
+++ project/server/office-queue/src/main/java/com/group11/office_queue/configs/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.group11.office_queue.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("http://example.com") // Allow specific origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // Allow credentials
+            }
+        };
+    }
+}
